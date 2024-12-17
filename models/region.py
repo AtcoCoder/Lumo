025b95940214ffd4a2eba03e600c77a2bd0e7e12
Omar Jammeh <orms45@gmail.com>
2024-12-17 16:24:56 +0000

    Add region class

--- models/region.py
+++ models/region.py
@@ -1,7 +1,15 @@
 """Region Class Module"""
 from models.base_model import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, relationship
 
 
 class Region(BaseModel):
     """Region Class Model"""
     __tablename__ = 'regions'
+    name = mapped_column(String(16), nullable=False)
+    areas = relationship('Area', backref='region')
+
+    def __init__(self, **kwargs) -> None:
+        """Initialises region instance"""
+        super().__init__(**kwargs)
