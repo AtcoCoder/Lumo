566ca6cd601c1963cb69c5217701b47c1cef265c
Omar Jammeh <orms45@gmail.com>
2024-12-20 16:05:13 +0000

    Add migrate

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 """app module"""
 from flask import Flask
 from config import DevelopmentConfig
+from models.base_model import Base
+from flask_migrate import Migrate
 
 
 def create_app(config_object=DevelopmentConfig):
@@ -10,10 +12,12 @@ def create_app(config_object=DevelopmentConfig):
     from api.v1.views import app_views
     app.register_blueprint(app_views)
 
+
     return app
 
 
 app = create_app()
+migrate = Migrate(app, Base)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
