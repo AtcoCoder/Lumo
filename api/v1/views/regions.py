fce2f3decf7d29d2f1b4dde34caf3d9e5fb163fa
Omar Jammeh <orms45@gmail.com>
2024-12-23 00:37:05 +0000

    Add add,update and delete region routes

--- api/v1/views/regions.py
+++ api/v1/views/regions.py
@@ -16,3 +16,35 @@ def add_region():
     )
     region.save()
     return jsonify(message='Region successfully added')
+
+@app_views.route('/regions')
+def get_regions():
+    """Get all regions route"""
+    regions = Region.get_all()
+    return jsonify(regions=regions)
+
+
+@app_views.route('/regions/regions_id', strict_slashes=False)
+def get_region(region_id):
+    """Get region route"""
+    region = region.get(region_id)
+    if not region:
+        return jsonify(message="Region Not Found"), 400
+
+@app_views.route(
+    '/regions/<region_id>',
+    methods=['DELETE', 'PATCH'],
+    strict_slashes=False
+)
+def w_regions(region_id):
+    """Delete/Update region route"""
+    region = Region.get(region_id)
+    if not region:
+        return jsonify(message="Region Not Found"), 400
+    if request.method == 'DELETE':
+        region.delete()
+        return jsonify(message='Successfully deleted')
+    to_update = ['name']
+    to_updates = region.get_infos_to_update(request, to_update)
+    region.update(**to_updates)
+    return jsonify(message='Succesfully updated')
