f32ca5654b8017af750b646a1bd7c32401e71f02
Omar Jammeh <orms45@gmail.com>
2025-01-03 11:02:39 +0000

    Add signup and login pages

--- home.py
+++ home.py
@@ -6,3 +6,22 @@ from flask import render_template
 @app.route('/', strict_slashes=False)
 def home():
     return render_template('index.html')
+
+@app.route(
+    '/login',
+    strict_slashes=False,
+    methods=['GET', 'POST']
+)
+def login():
+    """login page"""
+    return 'Login'
+
+
+@app.route(
+    '/signup',
+    strict_slashes=False,
+    methods=['POST', 'GET']
+)
+def signup():
+    """signup page"""
+    return 'Sign Up'
