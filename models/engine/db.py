0a568bdb2d6643eaaf8feb9f253bc226e31c2f83
Omar Jammeh <orms45@gmail.com>
2025-01-14 12:21:52 +0000

    feat: update DB class increase the connection pool

--- models/engine/db.py
+++ models/engine/db.py
@@ -1,5 +1,6 @@
 """db module"""
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError, InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.orm.exc import UnmappedInstanceError
@@ -40,6 +41,22 @@ class DB():
             pool_recycle=3600,
             pool_timeout=30
         )
+
+    def check_session(self):
+        """Ensure the is active."""
+        try:
+            self.__session.execute(text("SELECT 1"))
+        except (OperationalError, InvalidRequestError):
+            self.__session.rollback()
+            self.__session.close()
+            self.reload()
+
+    def rollback_on_error(self):
+        """Rollback the session if it's in a pending state."""
+        try:
+            self.__session.rollback()
+        except InvalidRequestError:
+            pass
     
     @property
     def session(self):
@@ -65,9 +82,14 @@ class DB():
         members of the table specified
         """ 
         if cls:
-            objs = self.__session.query(cls).all()
+            self.check_session()
 
-            return objs
+            try:
+                objs = self.__session.query(cls).all()
+
+                return objs
+            except OperationalError as e:
+                self.rollback_on_error()
         else:
             raise TypeError('Class must be specified')
     
@@ -79,12 +101,22 @@ class DB():
         valid_obj = [obj for cls in class_list if isinstance(obj, cls)]
         if not valid_obj:
             raise TypeError('Not a valid object')
-        self.__session.add(obj)
+        self.check_session()
+        try:
+            self.__session.add(obj)
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
     
     def save(self):
         """Commit all changes of the current session
         """
-        self.__session.commit()
+        self.check_session()
+        try:
+            self.__session.commit()
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
 
     def close(self):
         """Closes the current session
@@ -101,34 +133,51 @@ class DB():
             raise TypeError('id must be a string')
         
 
-        result = self.__session.query(cls).filter_by(id=id).first()
-        return result
+        self.check_session()
+        try:
+            result = self.__session.query(cls).filter_by(id=id).first()
+            return result
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
     
     def count(self, cls=None):
         """Count the number of members of 
         table(cls) and return the number
         """
-        results = self.all(cls)
-        return len(results)
+        self.check_session()
+        try:
+            results = self.all(cls)
+            return len(results)
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
     
     def get_by(self, cls, value, column=None):
         """Gets user by email or username"""
-        if not column:
-            result = self.__session.query(cls).filter_by(name=value).first()
-        elif column == 'email':
-            result = self.__session.query(User).filter_by(email=value).first()
-        elif column == 'username':
-            result = self.__session.query(User).filter_by(username=value).first()
-        else:
-            result = self.__session.query(BlockedToken).filter_by(jti=value).first()
-        return result
+        self.check_session()
+        try:
+            if not column:
+                result = self.__session.query(cls).filter_by(name=value).first()
+            elif column == 'email':
+                result = self.__session.query(User).filter_by(email=value).first()
+            elif column == 'username':
+                result = self.__session.query(User).filter_by(username=value).first()
+            else:
+                result = self.__session.query(BlockedToken).filter_by(jti=value).first()
+            return result
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
     
     def delete(self, obj):
         """Deletes object from the database"""
+        self.check_session()
         try:
             self.__session.delete(obj)
-        except Exception:
-            pass
+        except OperationalError as e:
+            self.rollback_on_error()
+            raise e
     
     def drop(self):
         """Drops all tables"""
