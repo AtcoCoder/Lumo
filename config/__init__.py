fb9affbf56e87b1727063e98c546bc432a798d94
Omar Jammeh <orms45@gmail.com>
2025-01-15 23:13:37 +0000

    feat: use env for config selection

--- config/__init__.py
+++ config/__init__.py
@@ -16,6 +16,7 @@ class ProductionConfig(Config):
     DATABASE_URL = os.getenv('DATABASE_URL')        
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    ADMIN_ID = os.getenv('ADMIN_ID')
 
 
 class DevelopmentConfig(Config):
